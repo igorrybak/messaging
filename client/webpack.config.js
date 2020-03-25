9bdae76823cfe785a558f8e1c32aa8459edb0f74
Igor Rybak <zathershtein@gmail.com>
2020-03-25 21:24:09 +0200

    Initial client

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,28 +1,28 @@
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 const path = require("path");
 
-const isDevServer = process.argv.find(v => v.includes("webpack-dev-server"));
-
-require("dotenv").config({
-    path: isDevServer ? "./.env" : "./.prod.env",
-});
+require("dotenv").config({ path: "./.env" });
 
 module.exports = {
     entry: {
         bundle: "./src/index.tsx",
     },
+    
     mode: process.env.NODE_ENV || "development",
+
     output: {
         publicPath: "/dist",
         filename: "main.js",
         path: path.join(process.cwd(), "/dist"),
     },
+
     devtool: "source-map",
 
     resolve: {
         plugins: [new TsconfigPathsPlugin()],
         extensions: [".ts", ".tsx", ".js", ".json"],
     },
+
     module: {
         rules: [
             {
@@ -33,7 +33,7 @@ module.exports = {
                 enforce: "pre",
                 test: /\.js*/,
                 loader: "source-map-loader",
-                exclude: [/node_modules/, /build/, /__test__/],
+                exclude: [/node_modules/, /build/],
             },
             {
                 test: /\.css$/,
