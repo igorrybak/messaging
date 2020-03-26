9b14c5f11bd956a2fe9e164a986602afbfabcf6e
Igor Rybak <zathershtein@gmail.com>
2020-03-26 17:49:52 +0200

    Axios beginning

--- client/src/components/signinup.tsx
+++ client/src/components/signinup.tsx
@@ -1,14 +1,27 @@
 import * as React from "react";
 import { TextField, Button } from "@material-ui/core";
 import axios from "axios";
+import { apiUrl } from "@/config";
 
 export const SignUpSignIn = (props: { action: string }) => {
     const buttonText = props.action === "sign-in" ? "Sign in" : "Create account";
+    const [userName, setUserName] = React.useState("");
+    const [password, setPassword] = React.useState("");
 
     return (
         <form noValidate autoComplete="off" onSubmit={requestToServer}>
-            <TextField id="standard-basic" label="Login" variant="outlined" />
-            <TextField id="standard-basic" label="Password" variant="outlined" />
+            <TextField
+                id="standard-basic"
+                label="Username"
+                variant="outlined"
+                onChange={e => setUserName(e.target.value)}
+            />
+            <TextField
+                id="standard-basic"
+                label="Password"
+                variant="outlined"
+                onChange={e => setPassword(e.target.value)}
+            />
             <Button type="submit" variant="contained" color="primary">
                 {buttonText}
             </Button>
@@ -16,8 +29,21 @@ export const SignUpSignIn = (props: { action: string }) => {
     );
 
     async function requestToServer(event: React.FormEvent<HTMLFormElement>) {
+        const createUser = props.action === "sign-in" ? false : true;
+        const params = { userName, password, createUser };
+        
         event.preventDefault();
-        const resp = await axios.get("http://127.0.0.1:3000/api/")
-        console.log(resp);
+
+        try {
+            const resp = await axios.get(apiUrl + "/messages", {
+                params,
+            });
+
+            console.log("Response: ", resp.status);
+        } catch (error) {
+            console.log("Error is occured:\n", error);
+        }
+
+        console.log("Sending params: ", params);
     }
 };
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,9 @@
 import * as express from "express";
+import * as bodyParser from "body-parser";
 
 export const router = express.Router();
 
-router.get("/", (req, resp) => {
+router.get("/messages", (req, resp) => {
+    console.debug("Received data: ", req.query);
     resp.send("Hello API!");
 });
